a72b772b24dd6c46c53b368e48ab7b5e3f375eb7
SampsonNg <sampsonngqianhao@hotmail.com>
2018-05-14 14:40:58 +0800

    basic interface all pages

--- js/rocket3.js
+++ js/rocket3.js
@@ -1,20 +1,30 @@
-var countdown = null;
+var timer = null;
 
 var changepage = function(page){
 	document.body.className = "a"+ page;
-	clearInterval(countdown);
+	clearInterval(timer);
 	var countdownnumber = 10;
 	document.getElementById('countdown').innerHTML = countdownnumber;
 
 	if (page == 2) {
-		countdown = setInterval(function(){
-			var countdownnumber = 10;
-			document.getElementById('countdown').innerHTML = countdownnumber;
+		var countdownnumber = 10;
+		timer = setInterval(function(){
 			countdownnumber = countdownnumber-1;
+			document.getElementById('countdown').innerHTML = countdownnumber;
 
-			if (countdownnumber == 0) {
-			changepage(3);
+			if(countdownnumber==0){
+				changepage(3);
 			};
 		},200);
-	}else if (page == 3) {}
+	}else if (page == 3) {
+		var success = setTimeout(function(){
+			var randomnumber = Math.round(Math.random()*100)
+			console.log('The random number was ' + randomnumber + '.')
+			if (randomnumber >= 50) {
+				changepage(4);
+			}else{
+				changepage(5);
+			}
+		},4000);
+	};
 }
